4ba026e6c57337c8151b337125c9a9e99f5343be
syedMohtashim <s.mohtashim.a@gmail.com>
2020-08-13 21:04:11 +0500

    Wrote a program to find a perfect-square

---
+++ leet_code/leet_code.py
@@ -0,0 +1,17 @@
+#To check whether a number is a Perfect square of 4
+def is_power(num):
+    if(num == 0):
+        return print(False)
+    while(num!= 1):
+        if(num%4 !=0):
+            return print(False)
+        num = num//4
+    return print(True)
+    # if(num == 0):
+    #    return print(False)
+    # while(num!=1):
+    #     if(num%4!=0):
+    #         return print(False) 
+    #     num = num//4
+    # return print(True)
+is_power(4)
